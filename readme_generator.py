0c2050380c8ec50dd48f59bea5fc25fe1701ff0e
Jimit Dholakia <jimitd101@gmail.com>
2023-12-18 10:54:12 -0800

    Update medium feed link and add last updated badge

--- readme_generator.py
+++ readme_generator.py
@@ -16,8 +16,8 @@ time.tzset()
 
 #updated  ='Last Updated on: ' + time.strftime('%b %d, %Y %X %Z', time.localtime())
 current_time = time.strftime('%b %d, %Y %X %Z', time.localtime())
-# updated = '![Last Updated](https://img.shields.io/badge/Last%20Updated%20on-' + \
-#    urllib.parse.quote(current_time) + '-brightgreen)'
+updated = '![Last Updated](https://img.shields.io/badge/Last%20Updated%20on-' + \
+   urllib.parse.quote(current_time) + '-brightgreen)'
 
 with open('intro.md') as f:
     intro = f.read()
@@ -30,7 +30,7 @@ with open('certifications.md') as f:
 with open('links.md') as f:
     links = f.read()
 
-feed_url = 'https://medium.com/feed/@jimit105'
+feed_url = 'https://jimit105.medium.com/feed'
 feed = feedparser.parse(feed_url)
 
 articles = ''
